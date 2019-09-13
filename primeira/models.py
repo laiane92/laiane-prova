81325eddeccc074e2074331028ab533ebf532104
laiane92 <anaseokjin15@gmail.com>
2019-09-13 19:25:02 -0300

    Segunda parte

--- primeira/models.py
+++ primeira/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+class Cliente(models.Model):
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=100)
+
+class Produto (models.Model):
+    nome=models.CharField(max_length=50)
+    categoria= models.CharField(max_length=50)
+    cliente=models.OneToOneField(Cliente, on_delete=models.CASCADE)
+class CarrinhoDeCompras (models.Model):
+    nome=models.CharField(max_length=10)
+    sexo=models.CharField(max_length=20)
+    produto=models.ForeignKey(Produto, on_delete=models.CASCADE)
+    
+
 # Create your models here.
